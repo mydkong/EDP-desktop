08804c938198ad8d2d145820758ba771ba1a2c3c
mydkong <contact.donkeykong@protonmail.com>
2025-02-17 10:31:12 +0100

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 # This is coded by mydkong
 
+cd $HOME/.local/share/applications/
+wget https://raw.githubusercontent.com/mydkong/EDP-desktop/refs/heads/main/linux/dp/plus.ecole-directe.desktop
+wget https://raw.githubusercontent.com/mydkong/EDP-desktop/refs/heads/main/linux/dp/icon.png
+xdg-open https://ecole-directe.plus
+# End of file
